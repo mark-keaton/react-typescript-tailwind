bc0b2488c3d6594c2e55e92fc34ceea109105248
Mark Keaton <mkeaton@gmail.com>
2021-12-13 11:13:35 -0600

    Initial commit concurrently building tailwind and react.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.svg";
+import Exercise from "./Exercise";
+
+function App() {
+  return <Exercise />;
+}
+
+export default App;
---
+++ src/Exercise.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+
+function Exercise() {
+  return (
+    <>
+      <h1 className="">This is a heading</h1>
+      <p className="">This is a paragraph</p>
+      <p className="">This is a paragraph</p>
+    </>
+  );
+}
+
+export default Exercise;
